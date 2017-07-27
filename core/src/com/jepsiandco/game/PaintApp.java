165b8dca0dab2cacc148af9983472794b62c5a42
Jepsiko <Jepsiko@Gmail.com>
2017-07-27 23:25:46 +0200

    Loading the Assets

--- core/src/com/jepsiandco/game/PaintApp.java
+++ core/src/com/jepsiandco/game/PaintApp.java
@@ -2,32 +2,38 @@ package com.jepsiandco.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PaintApp extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private Texture img;
+	private Texture dropImage, bucketImage;
+	private Sound dropSound;
+	private Music rainMusic;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("bucket.png");
+	    // Load the images for the droplet and the bucket, 64x64 pixels each
+		dropImage = new Texture("droplet.png");
+		bucketImage = new Texture("bucket.png");
+
+		// Load the drop sound effect and the rain background "music"
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+
+        // Start the playback of the background music immediately
+        rainMusic.setLooping(true);
+        rainMusic.play();
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
