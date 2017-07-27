9bb921535de19e24d4e9ad5e27d6a8976722a788
Jepsiko <Jepsiko@Gmail.com>
2017-07-28 00:28:26 +0200

    The Game Class

--- core/src/com/jepsiandco/game/PaintApp.java
+++ core/src/com/jepsiandco/game/PaintApp.java
@@ -1,140 +1,27 @@
 package com.jepsiandco.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
 
-import java.util.Iterator;
-
-public class PaintApp extends ApplicationAdapter {
-    private OrthographicCamera camera;
-    private SpriteBatch batch;
-
-    // Assets
-	private Texture dropImage, bucketImage;
-	private Sound dropSound;
-	private Music rainMusic;
-
-	// Objects
-    private Rectangle bucket;
-    private Array<Rectangle> raindrops;
-
-    // Other Variables
-    private Vector3 touchPos;
-    private long lastDropTime;
+public class PaintApp extends Game {
+    public SpriteBatch batch;
+    public BitmapFont font;
 	
 	@Override
 	public void create () {
-
-        // Create the camera and batch
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 800, 480);
         batch = new SpriteBatch();
-
-	    // Load the images for the droplet and the bucket, 64x64 pixels each
-		dropImage = new Texture("droplet.png");
-		bucketImage = new Texture("bucket.png");
-
-		// Load the drop sound effect and the rain background "music"
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
-        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-
-        // Start the playback of the background music immediately
-        rainMusic.setLooping(true);
-        rainMusic.play();
-
-        // Create the bucket
-        bucket = new Rectangle();
-        bucket.x = 800 / 2 - 64 / 2;
-        bucket.y = 20;
-        bucket.width = 64;
-        bucket.height = 64;
-
-        // Instantiate other variables
-        touchPos = new Vector3();
-        raindrops = new Array<Rectangle>();
-        spawnRaindrop();
+        font = new BitmapFont();
+        this.setScreen(new MainMenuScreen(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		camera.update();
-
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for (Rectangle raindrop : raindrops) {
-		    batch.draw(dropImage, raindrop.x, raindrop.y);
-        }
-		batch.end();
-
-		// Move the bucket with the touch or the mouse
-		if (Gdx.input.isTouched()) {
-            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            camera.unproject(touchPos);
-            bucket.x = touchPos.x - 64 / 2;
-        }
-
-        // Move the bucket with the keyboard
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-
-		// Keep the bucket inside the screen
-		if (bucket.x < 0) bucket.x = 0;
-		if (bucket.x > 800 - 64) bucket.x = 800 - 64;
-
-		// Create a new raindrop if enough time passed
-        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-
-        Iterator<Rectangle> iter = raindrops.iterator();
-        while (iter.hasNext()) {
-            Rectangle raindrop = iter.next();
-
-            // Move the raindrops
-            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-            if (raindrop.y + 64 < 0) iter.remove();
-
-            // Check collision between a raindrop and the bucket
-            if (raindrop.overlaps(bucket)) {
-                dropSound.play();
-                iter.remove();
-            }
-        }
+	    super.render();
 	}
 	
 	@Override
 	public void dispose () {
-	    dropImage.dispose();
-	    bucketImage.dispose();
-	    dropSound.dispose();
-	    rainMusic.dispose();
 	    batch.dispose();
 	}
-
-    /**
-     * Spawn a raindrop at a random 'x' and add it to the raindrops array
-     */
-    private void spawnRaindrop() {
-	    Rectangle raindrop = new Rectangle();
-	    raindrop.x = MathUtils.random(0, 800-64);
-	    raindrop.y = 480;
-	    raindrop.width = 64;
-	    raindrop.height = 64;
-	    raindrops.add(raindrop);
-	    lastDropTime = TimeUtils.nanoTime();
-    }
 }
