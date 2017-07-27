2055a918d8728368ebe462b3b8791722ec608618
Jepsiko <Jepsiko@Gmail.com>
2017-07-27 23:08:25 +0200

    Configurating Desktop Starter Class

--- desktop/src/com/jepsiandco/game/desktop/DesktopLauncher.java
+++ desktop/src/com/jepsiandco/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.jepsiandco.game.PaintApp;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+
 		new LwjglApplication(new PaintApp(), config);
 	}
 }
